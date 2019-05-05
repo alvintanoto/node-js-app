ad4e9216eaab16082cc45955e23a7fa2211be2ac
Alvin Tanoto <alv241197@outlook.com>
2019-05-05 15:37:55 +0700

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+var counter = require('./counter');
+
+console.log(counter.counter(['a', 'b', 'c']));
+console.log(counter.counter2(['a', 'b', 'c']));
---
+++ counter.js
@@ -0,0 +1,11 @@
+var counter = function(arr){
+    return `there are ${arr.length} elements in this array`;
+};
+
+var counter2 = function(arr){
+    return `test ${arr.length}`;
+};
+
+module.exports = {
+    counter, counter2
+}
