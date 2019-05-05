fa565213dfec77610d6d20e777ac0be68ca00ae3
Alvin Tanoto <alv241197@outlook.com>
2019-05-05 16:57:27 +0700

    server and pipe

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const http = require('http'); //server
 var config = require('./config/config'); //config
 var fs = require('fs'); //file or folder 
 
-/* create a server /
+/* create a server 
+
 const server = http.createServer(function(req,res){
     console.log(`request was made ${req.url}`);
     res.writeHead(200, {'Content-Type': 'text/plain'})
@@ -14,6 +15,8 @@ console.log(`now listening to port ${config.port}`);
 */
  
 
+/* read and write stream
+
 var myReadStream = 
     fs.createReadStream(`${__dirname}/readme.txt`, `utf8`);
 var myWriteStream = 
@@ -24,3 +27,24 @@ myReadStream.on('data', function(chunk){
     console.log(chunk);
     myWriteStream.write(chunk);
 }) 
+*/
+
+
+/* pipe
+
+myReadStream.pipe(myWriteStream);
+ */
+
+
+ /* server and pipe */
+
+const server = http.createServer(function(req,res){
+    console.log(`request was made ${req.url}`);
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    var myReadStream = 
+        fs.createReadStream(`${__dirname}/readme.txt`, `utf8`);
+    myReadStream.pipe(res);
+});
+
+server.listen(config.port, config.base_url);
+console.log(`now listening to port ${config.port}`);
