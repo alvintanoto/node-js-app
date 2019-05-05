955631f4648a418cf5f9d67b42067986873900a0
Alvin Tanoto <alv241197@outlook.com>
2019-05-05 16:20:01 +0700

    server init

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var counter = require('./counter');
+const http = require('http');
+var config = require('./config/config');
 
-console.log(counter.counter(['a', 'b', 'c']));
-console.log(counter.counter2(['a', 'b', 'c']));
+const server = http.createServer(function(req,res){
+    console.log(`request was made ${req.url}`);
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Hey !!');
+});
+
+server.listen(config.port, config.base_url);
+console.log(`now listening to port ${config.port}`);
+
